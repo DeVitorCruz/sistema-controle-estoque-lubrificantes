d8fec7fa5897604287bf6a5e7ae1fe59d2f0cc7c
DeVitor <97829726+DeVitorCruz@users.noreply.github.com>
2024-09-16 15:37:42 -0300

    Adicionando o caminho correto para o acesso do banco de dados

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///lubrificantes.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://lubrificantes_user:omOPGaeMSH8ACFTIJNbB4FpVdiXk6oTd@dpg-crk35d2j1k6c73am9u10-a.oregon-postgres.render.com/lubrificantes')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
